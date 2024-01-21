40f135bb6ea538dab68fb2ecf03ea5207e3c01c7
Dhiraj Udhani <dhirajudhani870@gmail.com>
2024-01-21 11:55:23 +0530

    vscode , hello world in html/js cdn , react hello world

---
+++ 01_Inception/Coding/App.js
@@ -0,0 +1,48 @@
+// Note = ReactElement(Object) => Html(Browser understand)
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//   },
+//   "Hello World in React!"
+// );
+
+// console.log(heading); // React element is normal js object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// Nested Dom
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "I am a h1 tag")
+//   )
+// );
+
+// root.render(parent)
+// console.log(parent)
+
+
+// Nested + sbilings
+
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+      [ React.createElement("h1", {}, "I am a h1 tag"), React.createElement("h2", {}, "I am a h2 tag")]
+    )
+  );
+
+  root.render(parent)
+   console.log(parent)
+  
