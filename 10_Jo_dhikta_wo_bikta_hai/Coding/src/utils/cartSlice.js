c0a69ef53628c7040ed8c32b12092518c96382a1
Dhiraj Udhani <dhirajudhani870@gmail.com>
2024-04-10 08:44:05 +0530

    debug reducer function

--- 10_Jo_dhikta_wo_bikta_hai/Coding/src/utils/cartSlice.js
+++ 10_Jo_dhikta_wo_bikta_hai/Coding/src/utils/cartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,current } from "@reduxjs/toolkit";
 
 
 const cartSlice = createSlice({
@@ -7,6 +7,14 @@ const cartSlice = createSlice({
         items: []
     },
     reducers:{
+        //Vanilla (older) Redux => DON'T HAVE TO MUTATE
+        // const newState = [...state]
+        // newState.items.push(action.payload)
+        //return newstate;
+
+
+        // Redux toolkit uses immer bts
+        // we have to mutate the state
         addItems : (state, action) => {
             // mutating our state here
             state.items.push(action.payload);
@@ -14,7 +22,18 @@ const cartSlice = createSlice({
         removeItems : (state) => {
             state.items.pop();
         },
+
+        //orignalState = ["pizza"]
         clearCart : (state) => {
+            // console.log(state);//[pizza]
+            // console.log(current(state))
+            // state = [] // [] but orignal state remain [pizza]
+            // console.log(state);
+
+
+            //RTK - either  mutate the existing state or return a new state
+
+            // orignalState = []
             state.items.length = 0; // makes items array clear with length 0 []
         }
     }
